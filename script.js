53a3b0b61b665595ab3786d97d72eb7f2996e72b
jad67tony <anthony.montmirail@gmail.com>
2020-01-31 20:11:52 +0100

    Prends en compte les années bisextiles, nettoyage du code

--- script.js
+++ script.js
@@ -3,11 +3,53 @@ const btn = document.getElementById('button');
 const divAge = document.getElementById('age');
 const result = document.getElementById('result');
 let radios = document.forms["formA"].elements["choix"];
+let bisextile = 0;
 let age = '';
 
-for (radio in radios) {radios[radio].onclick = function () { theSwitch(this.value);}}
-
+function secondes(age) {
+    result.innerHTML = lisibilite_nombre((age * 365 + bisextile) * 24 * 60 * 60);
+};
+function minutes(age) {
+    result.innerHTML = lisibilite_nombre(((age * 365 + bisextile) * 24) * 60);
+};
+function heures(age) {
+    result.innerHTML = lisibilite_nombre((age * 365 + bisextile) * 24);
+};
+function jours(age) {
+    result.innerHTML = lisibilite_nombre(age * 365 + bisextile);
+};
+btn.addEventListener('click', function () {
+    event.preventDefault();
+    age = divAge.value;
+    if (age === '') {
+        result.setAttribute("style", "red");
+        result.innerHTML = "Veuillez indiquer votre âge SVP";
+    } else {
+        theSwitch(radios.value);
+    }
+});
+function lisibilite_nombre(num) {
+    let result = num.toLocaleString();
+    return result;
+}
+for (radio in radios) {
+    radios[radio].onclick = function () {
+        theSwitch(this.value);
+    }
+}
+function bisextiles(age) {
+    if (bisextile != 0) {bisextile = 0;}
+    for (let i = 0; i < age; i++) {
+        if (i % 4 === 0) {
+            bisextile += 1;
+        } else {
+            bisextile += 0;
+        }
+    }
+    return bisextile;
+}
 function theSwitch(option) {
+    bisextiles(age);
     switch (option) {
         case 'option1':
             secondes(age);
@@ -23,72 +65,3 @@ function theSwitch(option) {
             break;
     }
 }
-function lisibilite_nombre(num) {
-    let result = num.toLocaleString();
-    return result;
-}
-function secondes(age) {
-    result.innerHTML = lisibilite_nombre((age * 365) * 24 * 60 * 60);
-};
-function minutes(age) {
-    result.innerHTML = lisibilite_nombre(((age * 365) * 24) * 60);
-};
-function heures(age) {
-    result.innerHTML = lisibilite_nombre((age * 365) * 24);
-
-};
-function jours(age) {
-    result.innerHTML = lisibilite_nombre(age * 365);
-};
-btn.addEventListener('click', function () {
-    event.preventDefault();
-    age = divAge.value;
-    if (age === '') {
-        result.setAttribute("style", "red");
-        result.innerHTML = "Veuillez indiquer votre âge SVP";
-    } else {
-        theSwitch(radios.value);
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-btn.addEventListener('click', function () {
-    event.preventDefault();
-    age = divAge.value;
-});
-divJours.addEventListener('click', function (age) {
-
-    console.log(age);
-
-});
-
-btn.addEventListener('click', e => {
-});
-btn.addEventListener('click', e => {
-});
-btn.addEventListener('click', e => {
-});
-*/
-
-
-
